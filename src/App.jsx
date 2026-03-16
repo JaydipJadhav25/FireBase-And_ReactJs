3615e826104bda5b9245c848310a6b64f641e575
JaydipJadhav25 <jaydipdjadhav@gmail.com>
2026-03-16 12:44:19 +0530

    feat: Send Notification From FireBasse

--- src/App.jsx
+++ src/App.jsx
@@ -3,85 +3,100 @@ import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import "./App.css";
 
-
-
 import { getDatabase, ref, set } from "firebase/database";
 
 //instace of firease app
-import { app } from "./FireBase";
+import { app  } from "./FireBase";
 import { useEffect, useState } from "react";
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
-import { getAuth, onAuthStateChanged  , signOut} from "firebase/auth"
-
-
-
-
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import { getMessaging, getToken } from "firebase/messaging";
 
 const db = getDatabase(app); //this is retrn instace of database
 
 //auth instance
 const auth = getAuth(app);
 
-function App() {
+//instace of messaging
+const messagingInstaces = getMessaging(app);
 
+function App() {
   const [user, setUser] = useState({
     id: null,
     name: null,
   });
 
-  const[currestUser , setCurrentUser] = useState(null);
+  const [currestUser, setCurrentUser] = useState(null);
 
-
-  function setUserData(obj){
+  function setUserData(obj) {
     //set state val
-    console.log("log : " , obj);
+    console.log("log : ", obj);
 
-    setUser(prev => {
-      return {...prev , [obj.target.name] : obj.target.value}
+    setUser((prev) => {
+      return { ...prev, [obj.target.name]: obj.target.value };
     });
-  };
-
-
-
-
+  }
 
   //function
   const putData = () => {
-      
     //check condition name is not null
-    if(user.name == null) alert("All data requried !");
+    if (user.name == null) alert("All data requried !");
 
-    console.log("user : " , user);
+    console.log("user : ", user);
 
     set(ref(db, `user/${user.name}`), {
-     ...user
+      ...user,
     });
-
   };
 
+  //to get access of web notifications
+  function requestPermission() {
+    console.log("Requesting permission...");
+    Notification.requestPermission().then((permission) => {
+      if (permission === "granted") {
+        console.log("Notification permission granted.");
+
+        //genrate token
+        getToken( messagingInstaces , { vapidKey: "BIUpU9HkA4-SkMPHuiQUjQLmt7UT0q4PhAmHUt0xgmvmEeJS4YJkhZWP3xE3jPL96L9zhoQWvELbwUJyryGze4c" })
+          .then((currentToken) => {
+            if (currentToken) {
+              console.log("user token : " , currentToken);
+            } else {
+              // Show permission request UI
+              console.log(
+                "No registration token available. Request permission to generate one.",
+              );
+            
+            }
+          })
+          .catch((err) => {
+            console.log("An error occurred while retrieving token. ", err);
+            // ...
+          });
+      } else if (permission === "denied") {
+        alert("Notification permission denied!");
+      }
+    });
+  }
 
   //check user is login or not
-  useEffect(()=>{
-  onAuthStateChanged(auth , user =>{
-    if (user) {
-      //yes user is logined
-      console.log("user is logined :" , user);
-      setCurrentUser(user);
-    }else{
-      //user => nulll
-      //so user is not logined out
-      console.log("user is not login :" , user);
-      setCurrentUser(null);
-    }
-  })
-  },[]);
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        //yes user is logined
+        console.log("user is logined :", user);
+        setCurrentUser(user);
+      } else {
+        //user => nulll
+        //so user is not logined out
+        console.log("user is not login :", user);
+        setCurrentUser(null);
+      }
+    });
 
-
-
-
-
-
+    requestPermission();
+  }, []);
 
   return (
     <>
@@ -93,39 +108,40 @@ function App() {
         </div>
 
         <h1>FireBase And Reactjs</h1>
-         <label htmlFor="">Id : </label>
-        <input type="number" name="id" onChange={e => setUserData(e)}/>
+        <label htmlFor="">Id : </label>
+        <input type="number" name="id" onChange={(e) => setUserData(e)} />
         <label htmlFor="">Name : </label>
-        <input type="text" name="name"  onChange={e => setUserData(e)}/>
+        <input type="text" name="name" onChange={(e) => setUserData(e)} />
 
-
-        <button onClick={()=>{
-          putData();
-        }}>create</button>
-        
+        <button
+          onClick={() => {
+            putData();
+          }}
+        >
+          create
+        </button>
       </section>
 
       <div className="ticks"></div>
       <section id="spacer">
         <h1> Firebase React Authentication</h1>
 
-        {
-          !currestUser ? <>
-          <Signup/>
-        <hr />
-        <Signin/>
-          </> : <>
-          <br/>
-            <h2>hello {currestUser?.displayName || "sir"}</h2>
-             
-             <h3>Email : {currestUser.email}</h3>
-
-
-             <button onClick={()=> signOut(auth)}>LogOut</button>
-
+        {!currestUser ? (
+          <>
+            <Signup />
+            <hr />
+            <Signin />
           </>
-        }
+        ) : (
+          <>
+            <br />
+            <h2>hello {currestUser?.displayName || "sir"}</h2>
 
+            <h3>Email : {currestUser.email}</h3>
+
+            <button onClick={() => signOut(auth)}>LogOut</button>
+          </>
+        )}
       </section>
     </>
   );
